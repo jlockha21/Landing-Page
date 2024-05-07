c7587dea54f101b1b1af1d7dc96e80d939ed1710
Jordan Lockhart <jordanlockhart556@gmail.com>
2024-05-06 23:08:14 -0700

    make it better

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
